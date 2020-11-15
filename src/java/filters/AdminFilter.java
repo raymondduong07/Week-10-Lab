dda108e2c1fcfe330ff469e831dca579c520ce22
rayfi <rayfi@DESKTOP-EUP6709.hitronhub.home>
2020-11-15 10:26:55 -0700

    Finished the admin filter

--- src/java/filters/AdminFilter.java
+++ src/java/filters/AdminFilter.java
@@ -4,9 +4,47 @@
  * and open the template in the editor.
  */
 package filters;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.User;
 /**
  *
  * @author Raymond
  */
-public class AdminFilter {
+public class AdminFilter implements Filter {
+    
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpSession session = httpRequest.getSession();
+        User user = (User) session.getAttribute("user");
+        
+        if (user.getRole().getRoleId() != 1) {
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+            httpResponse.sendRedirect("notes");
+            return;
+        }
+        
+        chain.doFilter(request, response);
+        
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+    
+    @Override
+    public void destroy() {
+    }
+
 }
